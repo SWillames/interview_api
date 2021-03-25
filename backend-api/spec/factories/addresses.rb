0a8354bfd2d6fcb106444beb2e0128aa7cd66e91
SWillames <sergiowoc@gmail.com>
2021-03-25 13:18:14 -0300

    Creating the model address.

---
+++ backend-api/spec/factories/addresses.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :address do
+    zip { "MyString" }
+    code { "MyString" }
+    street { "MyString" }
+    address { "MyString" }
+    neighborhood { "MyString" }
+    neighborhood { "MyString" }
+    uf { "MyString" }
+    ibge { "MyString" }
+    gia { "MyString" }
+    ddd { "MyString" }
+    siafi { "MyString" }
+    user { nil }
+  end
+end
