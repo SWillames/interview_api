30d9e2a5ccd79303670d16848012ddc78f4b5ba9
SWillames <sergiowoc@gmail.com>
2021-03-29 16:55:59 -0300

    factories finalized.

--- backend-api/spec/factories/addresses.rb
+++ backend-api/spec/factories/addresses.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :address do
-    zip_code { "MyString" }
-    street { "MyString" }
-    address { "MyString" }
-    neighborhood { "MyString" }
-    uf { "MyString" }
-    ibge { "MyString" }
-    gia { "MyString" }
-    ddd { "MyString" }
-    siafi { "MyString" }
-    user { nil }
+    
+    zip_code { (rand(01000000..99999999).to_s) }
+    street { Faker::Address.street_address }
+    neighborhood { "Center" }
+    uf { Faker::Address.state_abbr }
+    ibge { "54687" }
+    gia { "0045" }
+    ddd { "11" }
+    siafi { "7107" }
+    user { create(:user) }
   end
 end
