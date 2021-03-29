0132c61a8b6780a77eab38d2daebbfd93d72538f
SWillames <sergiowoc@gmail.com>
2021-03-29 15:59:18 -0300

    add the field complement to address table.

--- backend-api/db/schema.rb
+++ backend-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_25_171612) do
+ActiveRecord::Schema.define(version: 2021_03_26_030129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_03_25_171612) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "locality"
+    t.string "complement"
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
