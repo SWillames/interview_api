9fa007d819ee5ad44aa0d67bd3a4f7309521566b
SWillames <sergiowoc@gmail.com>
2021-03-29 16:50:57 -0300

    [u]cors.rb, fixing sintaxe error.

--- backend-api/config/initializers/cors.rb
+++ backend-api/config/initializers/cors.rb
@@ -2,8 +2,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
     resource '*',
-      expose: ['access-token', 'token-type', 'uid', 'client'],
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      :expose => ['access-token', 'token-type', 'uid', 'client'],
+      :headers => :any,
+      :methods => [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
