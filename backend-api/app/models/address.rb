d99367676d127bcb7da057db228ca43325e058c1
SWillames <sergiowoc@gmail.com>
2021-03-29 15:04:53 -0300

    testing models[address].

--- backend-api/app/models/address.rb
+++ backend-api/app/models/address.rb
@@ -1,3 +1,24 @@
 class Address < ApplicationRecord
   belongs_to :user
+  validates_presence_of :zip_code, :street, :neighborhood, :user
+  validates_format_of :zip_code, with: /\A.[0-9]+\z/
+  validates_length_of :zip_code, is: 8
+
+  def as_json(options = {})
+    ({
+      id: id,
+      zip_code: zip_code,
+      street: street,
+      neighborhood: neighborhood,
+      uf: uf,
+      ibge: ibge,
+      gia: gia,
+      ddd: ddd,
+      siafi: siafi,
+      user: user.name,
+      locality: locality,
+      complement: complement
+    })
+  end
+  
 end
--- backend-api/spec/models/address_spec.rb
+++ backend-api/spec/models/address_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "association" do
+    it { is_expected.to belong_to(:user,) }
+  end
+  
+
+  context "validaions" do
+    it { is_expected.to validate_presence_of(:zip_code) }
+    it { is_expected.to validate_presence_of(:street) }
+    it { is_expected.to validate_presence_of(:neighborhood) }
+    it { is_expected.to validate_presence_of(:user) }
+
+    %w[
+		abcdfgh
+		123456789ab
+		ab145879cxv
+		@1a254#$%a!
+		12345
+		invalid
+		].each do |zip_code|
+			it "requires #{zip_code} as invalid zip_code" do
+				address = Address.create(zip_code: zip_code)
+				expect(address.errors[:zip_code].size).to be >= 1
+			end
+    end
+    
+    %w[
+		123456789
+		1234567891011
+		154679584651
+		14896301746872
+		12345
+		].each do |zip_code|
+			it "requires #{zip_code} as invalid zip_code length" do
+				address = Address.create(zip_code: zip_code)
+				expect(address.errors[:zip_code].size).to be >= 1
+			end
+    end
+    
+    %w[
+		12345678
+		12345679
+		15467954
+		14896307
+		12345964
+		].each do |zip_code|
+			it "requires #{zip_code} as valid zip_code length" do
+				address = Address.create(zip_code: zip_code)
+				expect(address.errors[:zip_code].size).to eq 0
+			end
+		end
+  end
 end
